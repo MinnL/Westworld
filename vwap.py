1f288a1c7bb1d3795f155c13aaaa0448e75a7928
ying zhou <yz2490@nyu.edu>
2018-12-03 13:39:12 -0500

    Create vwap.py

---
+++ vwap.py
@@ -0,0 +1,16 @@
+#VWAP
+currenty_qty = 0
+current_VWAP = 0
+make_order = True
+while make_order:
+    new_qty = int(input("what's the qty?"))
+    price = float(input('price:'))
+    new_VWAP = (new_qty*price + currenty_qty*current_VWAP)/(currenty_qty+new_qty)
+    print(new_VWAP)
+    currenty_qty = currenty_qty + new_qty
+    current_VWAP = current_VWAP + new_VWAP
+
+
+
+
+
