efa9aba3f7fc1af85cec2290a8197ab8f98ba4e6
ying zhou <yz2490@nyu.edu>
2018-12-05 20:00:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def process_order():
 
 def get_connection():
     return mc.connect(user='root',
-    password='jigru8MySQL',
+    password='Odelia.0526',
     host='127.0.0.1',
     database='westworld',
     auth_plugin='mysql_native_password')
