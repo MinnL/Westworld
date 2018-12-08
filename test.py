d221fb7f4ff1c363740f145cc22bbfacfe930c38
ying zhou <yz2490@nyu.edu>
2018-12-08 03:56:19 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,71 +1,68 @@
-def insert_value():
-    connection = get_commection()
-    cursor = connection.cursor()
-
-    sql = "insert into "
-
-    symbol_id
-    inventory
-    WVAP
-    RPL
-    URPL
-
-
-
-
-
-
-sql_buy1 = ("select sum(qty) from trade where symbol_id = 1 and action = 'buy'")
-result = connection.cmd.query(sql_buy1)
-total_buy = connection.get_rows()
-total_buy = int(total_buy[0][0][0]) + int(qty)
-
-sql_sell1 = ("select sum(quantity) from trade where symbol_id = 1 and action = 'sell'")
-result = connection.cmd.query(sql_sell1)
-total_sell = connection.get_rows()
-total_sell = int(total_sell[0][0][0]) + int(qty)
-
-inventory = total_buy - total_sell
+def process_order2():
+    connection = get_connection()
+    qty = request.form['qty']
+    symbol = request.form.get('itemOrdered',type=int)
+    balance = get_balance()
+    if symbol == 1:
+      price = get_btc_sellprice()
+    elif symbol == 2:
+      price = get_eth_sellprice()
+    elif symbol == 3:
+      price = get_ltc_sellprice()
+    amount = float(price["amount"])
+    total_price = amount * int(qty)
 
 
 
-if symbol == 1:
-      price = get_btc_buyprice()  
-elif symbol == 2:
+
+    if qty <= inventory:
+        balance = balance + (amount * int(qty))
+        action = 'sell'
+        sql = 'insert into trade (qty,symbol_id,price,balance,action) values (%s, %s, %s, %s, %s)'
+        result = connection.cursor().execute(sql, (qty, symbol, amount, balance, action))
+         connection.commit()
+    else:
+      connection.close()
+      return render_template('notenoughmoney.html')
+    sql_pl = 'Update profit_loss Set symbol_id= %s, inventory= inventory+%s Where symbol_id=%s'
+    result_pl = connection.cursor().execute(sql_pl, (symbol, -qty, symbol))
+    connection.commit()
+    connection.close()
+    return render_template('ordersummary.html')
+
+    sql = 'insert into trade (qty,symbol_id,price,balance,action) values (%s, %s, %s, %s, %s)'
+    # i.e insert into orders (quantity, symbol_id) values (8000,2)
+    result = connection.cursor().execute(sql, (qty, symbol, amount, balance, action))
+    connection.commit()
+    connection.close()
+    return render_template('ordersummary.html')
+
+connection = get_connection()
+    qty = request.form['qty']
+    
+    symbol = request.form.get('itemOrdered',type=int)
+    balance = get_balance()
+    if symbol == 1:
+      price = get_btc_buyprice()
+    elif symbol == 2:
       price = get_eth_buyprice()
-elif symbol == 3:
+    elif symbol == 3:
       price = get_ltc_buyprice()
     amount = float(price["amount"])
-
-
-
-  # sql_buy1 = ("select sum(qty) from trade where symbol_id = 1 and action = 'buy'")
-    # result = connection.cmd_query(sql_buy1)
-    # total_buy = connection.get_rows()
-    # total_buy = int(total_buy[0][0][0]) + int(qty)
-
-    # sql_sell1 = ("select sum(qty) from trade where symbol_id = 1 and action = 'sell'")
-    # result = connection.cmd_query(sql_sell1)
-    # total_sell = connection.get_rows()
-    # total_sell = int(total_sell[0][0][0]) + int(qty)
-
-    # sql_buy2 = ("select sum(qty) from trade where symbol_id = 2 and action = 'buy'")
-    # result = connection.cmd_query(sql_buy2)
-    # total_buy = connection.get_rows()
-    # total_buy = int(total_buy[0][0][0]) + int(qty)
-
-    # sql_sell2 = ("select sum(qty) from trade where symbol_id = 1 and action = 'sell'")
-    # result = connection.cmd_query(sql_sell2)
-    # total_sell = connection.get_rows()
-    # total_sell = int(total_sell[0][0][0]) + int(qty)
-
-    sql_buy3 = ("select sum(qty) from trade where symbol_id = 3 and)
-    result = connection.cmd_query(sql_buy3)
-    total_buy = connection.get_rows()
-    total_buy = int(total_buy[0][0][0]) + int(qty)
-
-    inventory = total_buy - total_sell
-
-    inventory = str(inventory)
-
+    total_price = amount * int(qty)
     
+    
+    if total_price <= balance:
+      balance = balance - (amount * int(qty))
+      action = 'buy'
+      sql = 'insert into trade (qty,symbol_id,price,balance,action) values (%s, %s, %s, %s, %s)'
+      result = connection.cursor().execute(sql, (qty, symbol, amount, balance, action))
+      connection.commit()
+    else:
+      connection.close()
+      return render_template('notenoughmoney.html')
+    sql_pl = 'Update profit_loss Set symbol_id= %s, inventory= inventory+%s Where symbol_id=%s'
+    result_pl = connection.cursor().execute(sql_pl, (symbol, qty, symbol))
+    connection.commit()
+    connection.close()
+    return render_template('ordersummary.html')
