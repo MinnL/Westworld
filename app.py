8c00dee73aa323aa20ccb9c8cdba623d9c1d6cb1
ying zhou <yz2490@nyu.edu>
2018-12-12 14:55:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import request
+from flask import Markup
 from flask import render_template
 from coinbase.wallet.client import Client
 import mysql.connector as mc
@@ -8,20 +9,17 @@ app  = Flask(__name__)
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+  return render_template('login.html')
 
 # @app.route('/RPL')
 # def RPL():
 #     return render_template('RPL.html')
 
-@app.route('/UPL')
-def UPL():
-    connection = get_connection()
-    sql = "select trade_id , URPL from trade"
-    result = connection.cmd_query(sql)
-
-    
-    return render_template('UPL.html')
+# @app.route('/UPL')
+# def UPL():
+#     id = get_id
+#     totalUPL = get_totalUPL
+#     return render_template('UPL.html', x= id, y = totalUPL)
   
 
 
@@ -52,66 +50,64 @@ def westworld_main():
     ltcspotprice = get_ltc_spotprice()
     return render_template('main.html',pl=rows[0],stuff=symbol, stuffb=btcspotprice,stuffe=get_eth_spotprice(),stuffl=get_ltc_spotprice())
 
-@app.route('/graphs')
 
 @app.route('/ledger')
 def view_ledger():
-    connection = get_connection()
-    sql = "select * from trade order by trade_id desc"
-    result = connection.cmd_query(sql)
-    rows = connection.get_rows()
-    connection.close()
-    return render_template('ledger.html', ledgers=rows[0])
+   connection = get_connection()
+   sql = "select * from trade order by trade_id desc"
+   result = connection.cmd_query(sql)
+   rows = connection.get_rows()
+   connection.close()
+   return render_template('ledger.html', ledgers=rows[0])
 
 @app.route('/ordersummary1',methods=['POST'])
 def process_order1():
-    connection = get_connection()
-    qty = request.form['qty']
+  connection = get_connection()
+  qty = request.form['qty']
     
-    symbol = request.form.get('itemOrdered',type=int)
-    balance = get_balance()
-    if symbol == 1:
-      price = get_btc_buyprice()
-      mprice = get_btc_spotprice()
-    elif symbol == 2:
-      price = get_eth_buyprice()
-      mprice = get_eth_spotprice()
-    elif symbol == 3:
-      price = get_ltc_buyprice()
-      mprice = get_ltc_spotprice()
-    amount = float(price["amount"])
-    mprice = float(mprice["amount"])
-    total_price = amount * float(qty)
-    if total_price <= balance:
-      balance = balance - (amount * float(qty))
-      action = 'buy'
-      sql = 'insert into trade (qty,symbol_id,price,balance,action) values (%s, %s, %s, %s, %s)'
-      result = connection.cursor().execute(sql, (qty, symbol, amount, balance, action))
-      connection.commit()
-    else:
-      connection.close()
-      return render_template('notenoughmoney.html')
-    
-    inventory = get_inventory(symbol)
-    cvwap = get_vwap(symbol)
-    vwap1 = (total_price + inventory * cvwap)/ (inventory + float(qty))
-    RPL = 0
-    UPL = (mprice - vwap1) * (inventory+float(qty))
-
-    #for graph table
-    sql_graph = 'insert into graph (symbol_id,RPL,URPL) values (%s, %s, %s)'
-    result_graph = connection.cursor().execute(sql_graph, (symbol, RPL, UPL))
+  symbol = request.form.get('itemOrdered',type=int)
+  balance = get_balance()
+  if symbol == 1:
+    price = get_btc_buyprice()
+    mprice = get_btc_spotprice()
+  elif symbol == 2:
+    price = get_eth_buyprice()
+    mprice = get_eth_spotprice()
+  elif symbol == 3:
+    price = get_ltc_buyprice()
+    mprice = get_ltc_spotprice()
+  amount = float(price["amount"])
+  mprice = float(mprice["amount"])
+  total_price = amount * float(qty)
+  if total_price <= balance:
+    balance = balance - (amount * float(qty))
+    action = 'buy'
+    sql = 'insert into trade (qty,symbol_id,price,balance,action) values (%s, %s, %s, %s, %s)'
+    result = connection.cursor().execute(sql, (qty, symbol, amount, balance, action))
     connection.commit()
+  else:
+    connection.close()
+    return render_template('notenoughmoney.html')
+    
+  inventory = get_inventory(symbol)
+  cvwap = get_vwap(symbol)
+  vwap1 = (total_price + inventory * cvwap)/ (inventory + float(qty))
+  RPL = 0
+  UPL = (mprice - vwap1) * (inventory+float(qty))
+
+ 
+
+  #for graph table
+  sql_graph = 'insert into graph (symbol_id, RPL,URPL) values (%s,%s,%s)'
+  result_graph = connection.cursor().execute(sql_graph, (symbol,RPL, UPL))
+  connection.commit()
 
     #for profit_loss table
-    sql_pl = 'Update profit_loss Set symbol_id= %s, inventory= inventory+%s, vwap= %s, RPL =RPL+ %s, URPL=%s  Where symbol_id=%s'
-    result_pl = connection.cursor().execute(sql_pl, (symbol, qty, vwap1,RPL,UPL, symbol))
-    connection.commit()
-
-    
-
-    connection.close()
-    return render_template('ordersummary.html')
+  sql_pl = 'Update profit_loss Set symbol_id= %s, inventory= inventory+%s, vwap= %s, RPL =RPL+ %s, URPL=%s  Where symbol_id=%s'
+  result_pl = connection.cursor().execute(sql_pl, (symbol, qty, vwap1,RPL,UPL, symbol))
+  connection.commit()
+  connection.close()
+  return render_template('ordersummary.html')
 
 @app.route('/ordersummary2',methods=['POST'])
 def process_order2():
@@ -144,6 +140,8 @@ def process_order2():
     cvwap = get_vwap(symbol)
     RPL = (amount - cvwap) * float(qty)
     UPL = (mprice - cvwap) * (inventory - float(qty))
+   
+   
 
      #for profit_loss table
     if inventory < float(qty):
@@ -155,8 +153,8 @@ def process_order2():
       
 
      #for graph table
-    sql_graph = 'insert into graph (symbol_id, RPL, URPL) values (%s, %s, %s)'
-    result = connection.cursor().execute(sql_graph, (symbol, RPL, UPL))
+    sql_graph = 'insert into graph (symbol_id, RPL, URPL) values (%s,%s,%s)'
+    result = connection.cursor().execute(sql_graph, (symbol,RPL, UPL))
     connection.commit()
 
   
@@ -184,7 +182,7 @@ def sell():
 
 def get_connection():
     return mc.connect(user='root',
-    password='jigru8MySQL',
+    password='Odelia.0526',
     host='127.0.0.1',
     database='westworld',
     auth_plugin='mysql_native_password')
@@ -216,6 +214,21 @@ def get_vwap(x):
     cursor.execute("select vwap from profit_loss where symbol_id = %s", (x,))
     result = cursor.fetchone()
     return int(result[0])
+
+# def get_id():
+#     connection = get_connection()
+#     result = connection.cmd_query("select symbol_id from graph")
+#     rows = connection.get_rows()
+#     connection.close()
+#     return rows[0]
+
+# def get_totalUPL():
+#     connection = get_connection()
+#     result = connection.cmd_query("SELECT graph_id,@s := @s + URPL AS cumulative FROM graph CROSS JOIN (SELECT @s := 0) AS var ORDER BY graph_id")
+#     rows = connection.get_rows()
+#     connection.close()
+#     return rows[0]
+  
 
 # get buy price
 def get_btc_buyprice():
